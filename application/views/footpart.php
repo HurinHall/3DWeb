8407de5f73be0fe5e5d74206fa017585cf7dbda9
2010jing <2010jing@gmail.com>
2014-08-15 20:26:21 +0800

    delete all file to use new folder source

--- application/views/footpart.php
+++
@@ -1,6 +0,0 @@
-<!-- footer -->
-    <div id="footer">
-                <div class="indent">
-                    Copyright - Type in your name here  - Collect from <a href="#" title="3D website">3D website</a>
-                </div>
-    </div>
